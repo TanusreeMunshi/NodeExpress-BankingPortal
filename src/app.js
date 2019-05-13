0129ac400a0f2bdc565e4ad04aa1bc5be6f4afbf
Tanusree Munshi <M1043111@mindtree.com>
2019-05-13 16:13:36 +0530

    Completed the module

--- src/app.js
+++ src/app.js
@@ -4,8 +4,8 @@ const path = require('path');
 const express = require('express');
 const app=express();
 
-app.set('views',path.join(_dirname,'views'));
+app.set('views',path.join(__dirname,'views'));
 app.set('view engine','ejs');
-app.use(express.static(path.join(_dirname,'public')));
-app.get('/', (req,res)=>res.render('index',{titile: 'Index'}));
+app.use(express.static(path.join(__dirname,'public')));
+app.get('/', (req,res)=>res.render('index',{'title': 'Index'}));
 app.listen(3000, ()=> console.log('PS project Running on prot 3000!'));
