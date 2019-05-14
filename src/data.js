656e20da229838b7ac3ae9fe957a97d586c02de2
Tanusree Munshi <M1043111@mindtree.com>
2019-05-14 11:55:31 +0530

    Completed the module

--- src/data.js
+++ src/data.js
@@ -1,17 +1,19 @@
 const fs = require('fs');
 const path = require('path');
 
-const accounts = fs.readFileSync(
-    path.join(__dirname, 'json', 'accounts.json'), 'utf8'
+const accountData = fs.readFileSync(
+    path.join(__dirname,'json','accounts.json'),'utf8'
 );
+const accounts = JSON.parse(accountData);
 
-const users = fs.readFileSync(
-    path.join(__dirname, 'json', 'users.json'), 'utf8'
+const userData = fs.readFileSync(
+    path.join(__dirname,'json','users.json'),'utf8'
 );
+const users = JSON.parse(userData);
 
-const writeJSON = () => {
-    const accountsJSON = JSON.stringify(accounts, null, 4);
-    fs.writeFileSync(path.join(__dirname, 'json/accounts.json'), accountsJSON, 'utf8');
+const writeJSON = () =>{
+    const accountsJSON = JSON.stringify(accounts,null,4);
+    fs.writeFileSync(path.join(__dirname,'json/accounts.json'),accountsJSON,'utf8');
 };
 
-module.exports = { accounts, users, writeJSON }
+module.exports = {users,accounts,writeJSON}
